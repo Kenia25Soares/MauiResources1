4abafdf35f35b0ba3bcc36f63ef846765e9bedee
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-06-06 22:54:03 +0100

    Splitting the Bill App

---
+++ AppDividirConta/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppDividirConta
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
